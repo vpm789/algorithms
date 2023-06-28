a77ecbaa02dc9cc7e007481c86de8db635f6823c
vpm789 <vpm789@gmail.com>
2023-06-29 00:06:36 +0300

    algorithm3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,16 +8,21 @@ public class Main {
     public static void main(String[] args) {
         IntListImpl intList = new IntListImpl();
 
-        int[] arr1 = new int[100000];
-        for (int i = 0; i < arr1.length; i++) {
-            arr1[i] = (int) (Math.random() * 100);
-        }
 
+        int[] arr1 = new int[10];
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = (int) (Math.random() * 10);
+        }
         /*long start1 = System.currentTimeMillis();
         intList.sortSelection(arr1);
         System.out.println(System.currentTimeMillis() - start1);*/
 
-        System.out.println(intList.contains(arr1, 10));
+        System.out.println(intList.contains(arr1, 5));
+
+        /*intList.add(3);
+        intList.add(4);
+        intList.add(1);*/
+
     }
 
 }
